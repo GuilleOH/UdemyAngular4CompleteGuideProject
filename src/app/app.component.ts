74d85f835923068faac3473eccb5f39503a8d362
GuilleOH <guillermoortizherrera1987@gmail.com>
2017-12-07 17:49:10 +0100

    Seccion 20 acabada (Authentication & Route Protection in Angular Apps)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit{
+
+  ngOnInit(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyB7GtFuYtItbSQJtPn72XBvHQU7sHM_YT4",
+      authDomain: "ng-recipe-booki-4430a.firebaseapp.com",
+    });
+  }
+
 }
