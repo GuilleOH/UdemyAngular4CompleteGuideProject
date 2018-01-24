240a38d079d6db19e6e84ea94eac6aa808ea392c
GuilleOH <guillermoortizherrera1987@gmail.com>
2018-01-24 18:09:52 +0100

    Avances en sección 24 (13/46)

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ShoppingListService } from './shopping-list.service';
 import { Ingredient } from '../shared/ingredient.model';
 import { Observable } from 'rxjs/Observable';
 import { Store } from '@ngrx/store';
+import  * as fromShoppingList from './store/shopping-list.reducers';
+import  * as ShoppingListActions from './store/shopping-list.actions';
 
 
 
@@ -17,13 +18,14 @@ export class ShoppingListComponent implements OnInit {
 
   shoppingListState: Observable<{ingredients: Ingredient[]}>;
 
-  constructor(private slService: ShoppingListService, private store: Store<{shoppingList:{ingredients: Ingredient[]}}>) { }
+  constructor(private store: Store<fromShoppingList.AppState>) { }
 
   ngOnInit() {
     this.shoppingListState = this.store.select('shoppingList');
   }
 
   onEditItem(index:number){
-    this.slService.startedEditing.next(index);
+    this.store.dispatch(new ShoppingListActions.StartEdit(index));
+
   }
 }
